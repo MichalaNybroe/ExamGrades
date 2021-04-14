644a3a106e2f7f1953e0710452a9598320253ef8
Michala Nybroe <michalanim@gmail.com>
2021-04-14 13:49:59 +0200

    trying to use menu class for switch in main

--- ExamGrades/src/Main.java
+++ ExamGrades/src/Main.java
@@ -2,7 +2,11 @@ public class Main {
 
 
   public void run() {
+    String[] menuItems = new String[3];
+
     registerStudent();
   }
+
+
   
 }
---
+++ ExamGrades/src/Menu.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Menu {
+  private String menuHeader;
+  private String leadText;
+  private String[] menuItems;
+
+  Scanner scanner = new Scanner(System.in);
+
+  public Menu(String menuHeader, String leadText, String[] menuItems) {
+    this.menuHeader = menuHeader;
+    this.leadText = leadText;
+    this.menuItems = menuItems;
+  }
+
+  public String getMenuHeader() {
+    return menuHeader;
+  }
+
+  public String getLeadText() {
+    return leadText;
+  }
+
+  public String[] getMenuItems() {
+    return menuItems;
+  }
+
+  public void printMenu() {
+    System.out.println(getMenuHeader());
+    for (int i = 0; i < menuItems.length; i++) {
+      System.out.println(getMenuItems()[i]);
+    }
+    System.out.println(getLeadText());
+  }
+
+  public int readChoice() {
+    while (!scanner.hasNextInt()) {
+      System.out.println(getLeadText());
+      scanner.next();
+    }
+    return scanner.nextInt();
+  }
+}
