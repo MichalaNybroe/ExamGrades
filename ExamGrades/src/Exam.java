b0974fcd41117c5ef495ac75d6ea6e2f6411da23
Kamille Nikolajsen <kamillenikolajsen@Kamilles-MacBook-Pro-3.local>
2021-04-14 12:36:24 +0200

    In Exam class, changed constuctor and added getter and setter

--- ExamGrades/src/Exam.java
+++ ExamGrades/src/Exam.java
@@ -1,10 +1,30 @@
 public class Exam {
 
+  //ATTRIBUTES
   private String name;
   private int grade;
 
+  //CONSTRUCTOR
   public Exam(String name, int grade) {
+    setName(name);
+    setGrade(grade);
+  }
+
+  //GETTER
+  public String getName(){
+    return name;
+  }
+
+  public int getGrade(){
+  return grade;
+  }
+
+  //SETTER
+  public void setName(String name){
     this.name = name;
+  }
+
+  public void setGrade(int grade){
     this.grade = grade;
   }
 }
