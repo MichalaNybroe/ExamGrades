1ce58ca9754847ba823109a3866e5f8eb4f67ef1
Michala Nybroe <michalanim@gmail.com>
2021-04-14 12:55:16 +0200

    Student class made

---
+++ ExamGrades/src/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+  private String name;
+  private String studentID;
+
+ public void setName(String name) {
+   this.name = name;
+ }
+
+ public void setStudentID(String studentID) {
+
+ }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getStudentID() {
+   return studentID;
+  }
+}
